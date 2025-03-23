9517fcffe8ab008d29068d48e919a994f969aa33
kamelia918 <kameliaremaki@gmail.com>
2025-03-23 20:52:08 +0100

    fixed the logout

--- patient/urls.py
+++ patient/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path("register/", views.register, name="register"),
-
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'), # Redirect to home page after logout
     path("patientinformation/", views.patientinformation, name="patientinformation"),
     path("patientlist/", views.patientlist, name="patientlist"),
 
